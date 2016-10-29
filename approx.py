33b30c257b72303bab58842136c3aab7c439757a
neal-o-r <neal@n-o-r.xyz>
2016-10-29 22:25:17 +0100

    working version

--- approx.py
+++ approx.py
@@ -6,12 +6,29 @@ import matplotlib.pyplot as plt
 plt.style.use('fivethirtyeight')
 
 
-n_example = 100
+n_example = 1000
 n_batch   = 50
-n_epoch   = 1000
+n_epoch   = 300
+f_range   = (-5,5) 
 
 np.random.seed(123) 
-fx = lambda x: x + 0.1*np.random.randn(*x.shape)
+fx_true = lambda x: x**2 + x - 10
+fx = lambda x: fx_true(x) + 0.1*np.random.randn(*x.shape)
+
+def make_plot(train, guess):
+
+        xs = np.linspace(f_range[0], f_range[1], 100)
+
+        plt.plot(train[0], train[1], 'o', label='Training Data')
+        plt.plot(xs, fx_true(xs), label='True Function')
+        plt.plot(test[0], guesses, 'o', label='Test Data')
+        plt.legend(loc='best')
+        plt.xlim(1.2*f_range[0], 1.2*f_range[1])
+        plt.ylim(1.2*min(train[1])[0], 1.2*max(train[1])[0])
+
+        plt.show()
+
+
 
 def get_data(fx, n, x_range=[-5,5]):
 
@@ -30,9 +47,9 @@ def get_data(fx, n, x_range=[-5,5]):
 
 def approximator(x):
         
-        l0 = tf.nn.softplus(net.feed_forward(x, 10, 'approx', 'l0'))
+        l0 = tf.nn.softplus(net.feed_forward(x, 20, 'approx', 'l0'))
         l1 = net.feed_forward(l0, 1, 'approx', 'l1')
-                
+            
         return l1
 
 
@@ -44,20 +61,21 @@ Y = tf.placeholder(tf.float32, [1, 1], name="Y")
 y_c = approximator(X)
 
 cost = tf.nn.l2_loss(y_c - Y)
-train_op = tf.train.GradientDescentOptimizer(0.05).minimize(cost)
+train_op = tf.train.GradientDescentOptimizer(0.005).minimize(cost)
 
 guesses = []
-a = tf.Variable([[1.]]) 
 with tf.Session() as sess:
         
         sess.run(tf.initialize_all_variables())
-        res = net.feed_forward(net.feed_forward(a, 10, 'test', 't'), 1, 'test', 't1')
-'''
+
         for i in range(n_epoch):
+        
+                inds = np.random.random_integers(0, 
+                        len(train[0])-1, size=n_batch) 
 
-                x_b = np.random.choice(train[0].ravel(), size=n_batch)
-                y_b = np.random.choice(train[1].ravel(), size=n_batch)                
-
+                x_b = train[0].ravel()[inds]
+                y_b = train[1].ravel()[inds]               
+        
                 for x, y in zip(x_b, y_b):
                         
                         sess.run(train_op, feed_dict={X: np.array([[x]]), 
@@ -67,14 +85,13 @@ with tf.Session() as sess:
                         mse = 0.
                         for x, y in zip(test[0].ravel(), test[1].ravel()):
 
-                                mse += sess.run(tf.nn.l2_loss(y_c - y),  feed_dict={X:np.array([[x]])})
+                                v = sess.run(y_c, feed_dict={X:np.array([[x]])})
+                                mse += (v - y)**2
+
                         print("Epoch %i, MSE %g" %(i, mse))
         
         for x, y in zip(test[0].ravel(), test[1].ravel()):
 
                 guesses.append(sess.run(y_c,  feed_dict={X:np.array([[x]])})[0][0])
                         
-plt.plot(train[0], train[1], 'o') 
-plt.plot(test[0], guesses, 'o')
-plt.show()
-'''
+make_plot(train, guesses)
--- net.py
+++ net.py
@@ -19,13 +19,12 @@ def feed_forward(neurons, n_next_layer, scope, name):
         assert 1 in neurons.get_shape()
         if 1 != neurons.get_shape()[0]:
                 neurons = tf.transpose(neurons)
-
+        
         w_init = tf.random_normal_initializer(stddev=1.0)
         b_init = tf.constant_initializer(0.0)
 
-        w = get_scope_variable(scope, name, [neurons.get_shape()[1], n_next_layer], w_init)
-        b = get_scope_variable(scope, name, [n_next_layer], b_init)
-
+        w = get_scope_variable(scope, 'w'+name, [neurons.get_shape()[1], n_next_layer], w_init)
+        b = get_scope_variable(scope, 'b'+name, [n_next_layer], b_init)
+        
         return tf.matmul(neurons, w) + b
-
 
